d02c6932f22d57f1be228591a6b8950289529cae
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-12-12 20:22:23 +0200

    Created Dashboard component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 
+import Dashboard from './components/Dashboard';
 import Auth from './components/Auth';
 
 const App = () => {
   return (
     <>
       <Switch>
+        <PrivateRoute exact path="/" component={Dashboard} />
         <Route path="/auth" component={Auth} />
       </Switch>
     </>
