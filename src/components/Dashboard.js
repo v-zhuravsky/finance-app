ed3ac2ec98ace438cdd0630bfdfff9482f17b10f
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-12-13 04:13:00 +0200

    Used useTitle hook to change page title

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import useTitle from '../hooks/useTitle';
 
 import Layout from './Layout';
 
 const Dashboard = () => {
+  useTitle('Home page');
   return (
     <Layout>
       
