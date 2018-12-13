0a1637a0acb919f34d489351dd251e03b50df615
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-12-13 04:08:48 +0200

    Moved Dashboard charts to Stats component

---
+++ src/styles/Stats.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'aphrodite-jss';
+
+export const statsStyles = StyleSheet.create({
+  areaCharts: {
+    overflow: 'hidden',
+    '& div': {
+      float: 'left',
+      width: '50%'
+    }
+  }
+});
