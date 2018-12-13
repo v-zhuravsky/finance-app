84073f43b7b515f2bb2df2ff3418aa19b927f8c9
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-12-13 04:09:35 +0200

    Added PrivateRoute to Stats page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 
 import Dashboard from './components/Dashboard';
+import Stats from './components/Stats';
 import Auth from './components/Auth';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <>
       <Switch>
         <PrivateRoute exact path="/" component={Dashboard} />
+        <PrivateRoute path="/stats" component={Stats} />
         <Route path="/auth" component={Auth} />
       </Switch>
     </>
