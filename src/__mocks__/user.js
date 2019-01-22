79c1acb7811573fa26e66a8a50d5c8e5fad07b13
Viacheslav <Viacheslav Zhuravskyi>
2019-01-23 01:11:32 +0200

    chore: add some fields to user mock data

--- src/__mocks__/user.js
+++ src/__mocks__/user.js
@@ -3,5 +3,7 @@ export default {
   firstName: 'Viacheslav',
   lastName: 'Zhuravskyi',
   username: 'willson',
-  avatar: 'https://avatarfiles.alphacoders.com/865/86573.jpg'
+  avatar: 'https://avatarfiles.alphacoders.com/865/86573.jpg',
+  totalMoneySpent: 2435045,
+  spentThisMonth: 2533
 };
