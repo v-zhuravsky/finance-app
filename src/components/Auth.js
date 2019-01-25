536f02ea6c88b36c63570ffa634efe93c042f446
Viacheslav <Viacheslav Zhuravskyi>
2019-01-25 23:47:44 +0200

    feat(auth): implement basic auth functionality

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,31 +2,44 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { css } from 'aphrodite-jss';
+import authAction from '../actions/auth.ts';
+import useInput from '../hooks/useInput';
 
 import { authStyles } from '../styles/Auth';
 
-const Auth = ({ user }) => {
+const Auth = ({ user, authAction }) => {
   if (user.token) {
     if (user.token.length === 100) {
       return <Redirect to="/" />;
     }
   }
 
+  const login = useInput('');
+  const password = useInput('');
+
+  const handleAuth = e => {
+    e.preventDefault();
+
+    authAction(login.value, password.value);
+  };
+
   return (
     <div className={css(authStyles.authForm)}>
       <h1>Welcome back!</h1>
-      <form>
+      <form onSubmit={handleAuth}>
         <label htmlFor="login">Login</label>
         <input
           id="login"
           placeholder="Login"
           type="text"
+          {...login}
         />
         <label htmlFor="password">Password</label>
         <input
           id="password"
           placeholder="Password"
           type="password"
+          {...password}
         />
         <button>Authorize</button>
       </form>
@@ -40,4 +53,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(Auth);
+export default connect(mapStateToProps, { authAction })(Auth);
